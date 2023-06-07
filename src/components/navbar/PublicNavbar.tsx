65b0bd6147d1b61d01932daa7f06a0505e05be36
OlimUmarov <olimumarov2017@gmail.com>
2023-06-07 09:53:43 +0500

    update

--- src/components/navbar/PublicNavbar.tsx
+++ src/components/navbar/PublicNavbar.tsx
@@ -60,7 +60,7 @@ const PublicNavbar = () => {
                 <img
                   src="https://flowbite.com/docs/images/logo.svg"
                   className="mr-3 h-6 sm:h-9"
-                  alt="Flowbite Logo"
+                  alt="Awards logo"
                 />
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                   Tech Awards
