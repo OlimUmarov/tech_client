6975d674b1bd8e75144e7d0b8d8505acd3027887
OlimUmarov <olimumarov2017@gmail.com>
2023-06-30 15:58:55 +0500

    Title update

--- src/components/navbar/PrivateNavbar.tsx
+++ src/components/navbar/PrivateNavbar.tsx
@@ -64,10 +64,10 @@ const PrivateNavbar = () => {
                 <img
                   src="https://flowbite.com/docs/images/logo.svg"
                   className="mr-3 h-6 sm:h-9"
-                  alt="Flowbite Logo"
+                  alt="Mazmoon Logo"
                 />
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                  Tech Awards
+                Mazmoon
                 </span>
               </NavLink>
 
--- src/components/navbar/PublicNavbar.tsx
+++ src/components/navbar/PublicNavbar.tsx
@@ -60,10 +60,10 @@ const PublicNavbar = () => {
                 <img
                   src="https://flowbite.com/docs/images/logo.svg"
                   className="mr-3 h-6 sm:h-9"
-                  alt="Awards logo"
+                  alt="Mazmoon logo"
                 />
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                  Tech Awards
+                Mazmoon
                 </span>
               </NavLink>
 
